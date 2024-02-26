48549a93b23edb7fdb2efebcddec30341a6d0248
Mihunchik <arinushkin048@mail.ru>
2024-02-26 22:00:06 +0300

    end of third step

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
-console.log('hello ' + name);
+export function getName() {
+    console.log('May I have your name?');
+    const name = readlineSync.question();
+    console.log('Hello, ' + name + '!');
+}
