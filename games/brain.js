0ed8cc9cfebec9ee12f60689ed7ff79d59bd20f6
Mihunchik <arinushkin048@mail.ru>
2024-07-24 14:27:08 +0300

    transfer games

--- games/brain.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-import greeting from '../src/greeting.js';
-
-export default function brain() {
-  greeting();
-  const name = readlineSync.question();
-  console.log(`Hello, ${name}!`);
-}
