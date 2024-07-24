07c03d13601da64e924fc6c00d2f64c38bbb9ec6
Mihunchik <arinushkin048@mail.ru>
2024-07-24 14:51:37 +0300

    fix chek 0

--- src/games/prime.js
+++ src/games/prime.js
@@ -10,34 +10,47 @@ export default function prime() {
 
   for (let i = 0; i <= 2;) {
     const rnd = getRndNum();
-    const halfRnd = Math.ceil(rnd / 2);
-    let count = 0;
-    if (rnd === 1) {
-      count = 1;
-    }
-    for (let j = 2; j <= halfRnd + 1; j += 1) {
-      if (rnd % j === 0) {
-        count += 1;
+    if (rnd === 0) {
+      console.log('Question: 0');
+      console.log('Your answer: ');
+      const answ = readlineSync.question();
+      if (answ === 'no') {
+        console.log('Correct!');
+        i += 1;
+      } else {
+        console.log(`no is the wrong answer :(. Let's try again, ${name}!`);
+        break;
       }
-    }
-    console.log(`Question: ${rnd}`);
-    console.log('Your answer: ');
-    const answ = readlineSync.question();
-    if ((count > 0 && answ === 'no') || (count === 0 && answ === 'yes')) {
-      console.log('Correct!');
-      i += 1;
-    } else if (count === 0 && answ === 'no') {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!`);
-      break;
-    } else if (count > 0 && answ === 'yes') {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`);
-      break;
     } else {
-      console.log('error!');
-      break;
-    }
-    if (i > 2) {
-      console.log(`Congratulations, ${name}!`);
+      const halfRnd = Math.ceil(rnd / 2);
+      let count = 0;
+      if (rnd === 1) {
+        count = 1;
+      }
+      for (let j = 2; j <= halfRnd + 1; j += 1) {
+        if (rnd % j === 0) {
+          count += 1;
+        }
+      }
+      console.log(`Question: ${rnd}`);
+      console.log('Your answer: ');
+      const answ = readlineSync.question();
+      if ((count > 0 && answ === 'no') || (count === 0 && answ === 'yes')) {
+        console.log('Correct!');
+        i += 1;
+      } else if (count === 0 && answ === 'no') {
+        console.log(`no is the wrong answer :(. Correct answer was "yes". Let's try again, ${name}!`);
+        break;
+      } else if (count > 0 && answ === 'yes') {
+        console.log(`yes is the wrong answer :(. Correct answer was "no". Let's try again, ${name}!`);
+        break;
+      } else {
+        console.log('error!');
+        break;
+      }
+      if (i > 2) {
+        console.log(`Congratulations, ${name}!`);
+      }
     }
   }
 }
