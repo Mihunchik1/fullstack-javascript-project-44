7e61b1c1716fa2d71880b6aedb0960b60dfcff68
Mihunchik <arinushkin048@mail.ru>
2024-07-30 13:30:21 +0300

    returned back

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,18 @@
 import getRndNum from '../getRandomInRange.js';
 import runEngine from '../index.js';
 
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const generateRound = () => {
+  const result = [];
+  const question = getRndNum();
+  result.push(question);
+  if (question % 2 === 0) {
+    result.push('yes');
+  } else {
+    result.push('no');
+  }
+  return result;
+};
 export default function even() {
-  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const generateRound = () => {
-    const result = [];
-    const question = getRndNum();
-    result.push(question);
-    if (question % 2 === 0) {
-      result.push('yes');
-    } else {
-      result.push('no');
-    }
-    return result;
-  };
   runEngine(rules, generateRound);
 }
