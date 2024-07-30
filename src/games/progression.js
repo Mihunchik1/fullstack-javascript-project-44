3fb2cfd4eaddb4b4979c0478b1e577d1015f03bb
Mihunchik <arinushkin048@mail.ru>
2024-07-30 15:18:05 +0300

    final fix brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,51 +1,33 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
 import getRndNum from '../getRandomInRange.js';
+import runEngine from '../index.js';
 
-export default function progression() {
-  greeting();
-  const name = readlineSync.question();
-  console.log(`Hello, ${name}!`);
-  console.log('What number is missing in the progression?');
+const rules = 'What number is missing in the progression?';
+const generateRound = () => {
+  const result = [];
+  const rndArr = [];
 
-  for (let i = 0; i <= 2;) {
-    const rndArr = [];
+  const firstNum = getRndNum();
+  rndArr.push(firstNum);
 
-    const firstNum = getRndNum();
-    rndArr.push(firstNum);
+  const step = getRndNum();
 
-    const step = getRndNum();
-
-    for (let j = 1; j < 10; j += 1) {
-      const nextNum = rndArr[j - 1] + step;
-      rndArr.push(nextNum);
-    }
-
-    const copyRndArr = [...rndArr];
-    const rndKey = Math.floor(Math.random() * 10);
-
-    copyRndArr[rndKey] = '..';
-
-    const result = rndArr[rndKey];
-
-    const listArr = copyRndArr.join(' ');
-
-    console.log(`Question: ${listArr}`);
-    console.log('Your answer: ');
-    const answ = readlineSync.question();
-    if (result === Number(answ)) {
-      console.log('Correct!');
-      i += 1;
-    } else if (result !== Number(answ)) {
-      console.log(`${answ} is wrong answer ;(. Correct answer was ${result}.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    } else {
-      console.log('error!');
-      break;
-    }
-    if (i > 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
+  for (let j = 1; j < 10; j += 1) {
+    const nextNum = rndArr[j - 1] + step;
+    rndArr.push(nextNum);
   }
+
+  const copyRndArr = [...rndArr];
+  const rndKey = Math.floor(Math.random() * 10);
+
+  copyRndArr[rndKey] = '..';
+
+  const answer = rndArr[rndKey];
+
+  const question = copyRndArr.join(' ');
+  result.push(question);
+  result.push(answer.toString());
+  return result;
+};
+export default function progression() {
+  runEngine(rules, generateRound);
 }
