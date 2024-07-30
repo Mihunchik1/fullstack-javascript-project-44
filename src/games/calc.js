45a7f12963b72a987fb9712550956e69209b7c3a
Mihunchik <arinushkin048@mail.ru>
2024-07-30 14:12:41 +0300

    final fix brain-calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,50 +1,34 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
 import getRndNum from '../getRandomInRange.js';
+import runEngine from '../index.js';
+
+const rules = 'What is the result of the expression?';
+const generateRound = () => {
+  const result = [];
+  const arrOperator = ['+', '-', '*'];
+  const operator = arrOperator[Math.floor(Math.random() * arrOperator.length)];
+
+  const rnd1 = getRndNum();
+  const rnd2 = getRndNum();
+
+  const questionStr = `${rnd1} ${operator} ${rnd2}`;
+  result.push(questionStr);
+  let answerNum;
+  switch (operator) {
+    case '+':
+      answerNum = rnd1 + rnd2;
+      break;
+    case '-':
+      answerNum = rnd1 - rnd2;
+      break;
+    case '*':
+      answerNum = rnd1 * rnd2;
+      break;
+    default:
+  }
+  result.push(answerNum.toString());
+  return result;
+};
 
 export default function calculator() {
-  greeting();
-  const name = readlineSync.question();
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
-
-  for (let i = 0; i <= 2;) {
-    const arrOperator = ['+', '-', '*'];
-    const rndOperator = Math.floor(Math.random() * arrOperator.length);
-    const operator = arrOperator[rndOperator];
-
-    const rnd1 = getRndNum();
-    const rnd2 = getRndNum();
-    let result;
-
-    switch (operator) {
-      case '+':
-        result = rnd1 + rnd2;
-        break;
-      case '-':
-        result = rnd1 - rnd2;
-        break;
-      case '*':
-        result = rnd1 * rnd2;
-        break;
-      default:
-    }
-    console.log(`Question: ${rnd1} ${operator} ${rnd2}`);
-    console.log('Your answer: ');
-    const answ = readlineSync.question();
-    if (result === Number(answ)) {
-      console.log('Correct!');
-      i += 1;
-    } else if (result !== Number(answ)) {
-      console.log(`${answ} is wrong answer ;(. Correct answer was ${result}.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    } else {
-      console.log('error!');
-      break;
-    }
-    if (i > 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
+  runEngine(rules, generateRound);
 }
